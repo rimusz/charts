ecb5e75ce8105281d846226f3f6892795a491d77
rimas <rmocius@gmail.com>
2018-11-22 17:25:27 +0000

    fix kubeconfig for ct container

--- test/e2e-kind.sh
+++ test/e2e-kind.sh
@@ -9,18 +9,19 @@ readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 run_kind() {
 
     echo "Get kind binary..."
-    docker run --rm -it -v "$(pwd)":/go/bin golang go get sigs.k8s.io/kind
-    sudo mv kind /usr/local/bin/
+    #docker run --rm -it -v "$(pwd)":/go/bin golang go get sigs.k8s.io/kind && chmod +x kind && sudo mv kind /usr/local/bin/
+    curl -Lo kind https://storage.googleapis.com/rimusz-ci-artifacts/kind && chmod +x kind && sudo mv kind /usr/local/bin/
 
     echo "Create Kubernetes cluster with kind..."
-    kind create cluster
-}
+    kind create cluster --image=kindest/node:"$K8S_VERSION"
 
-set_kind() {
+    #echo "Download kubectl..."
+    #curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/"${K8S_VERSION}"/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+    #echo
 
     echo "Copy kubectl out of the kind container..."
     tmp_path=$(mktemp -d)
-    docker cp "$config_container_id":"$(docker exec "$config_container_id" which kubectl)" "${tmp_path}/kubectl" && sudo mv "${tmp_path}"/kubectl /usr/local/bin/
+    docker cp kind-1-control-plane :"$(docker exec kind-1-control-plane which kubectl)" "${tmp_path}/kubectl" && sudo mv "${tmp_path}"/kubectl /usr/local/bin/
     echo
 
     echo "Export kubeconfig..."
@@ -61,10 +62,13 @@ main() {
     # shellcheck disable=SC2064
     trap "docker rm -f $config_container_id > /dev/null" EXIT
 
-    set_kind
-
+    # Get kind container IP
+    kind_container_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' kind-1-control-plane)
     # Copy kubeconfig file
+    docker exec "$config_container_id" mkdir /root/.kube
     docker cp "$KUBECONFIG" "$config_container_id:/root/.kube/config"
+    # Update localhost to kind container IP
+    docker exec "$config_container_id" sed -i "s/localhost/$kind_container_ip/g" /root/.kube/config
     
     echo "Add git remote k8s ${CHARTS_REPO}"
     git remote add k8s "${CHARTS_REPO}" &> /dev/null || true
