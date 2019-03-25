f8e0095bf7481b7aa313f680256d2034853744ab
rimas <rmocius@gmail.com>
2019-03-25 16:39:11 +0000

    .

--- .test/lint-charts.sh
+++ .test/lint-charts.sh
@@ -4,10 +4,6 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-# shellcheck disable=SC2082
-readonly IMAGE_TAG=${CHART_TESTING_TAG}
-# shellcheck disable=SC2082
-readonly IMAGE_REPOSITORY=${$CHART_TESTING_IMAGE}
 readonly REPO_ROOT="${REPO_ROOT:-$(git rev-parse --show-toplevel)}"
 
 main() {
@@ -15,7 +11,7 @@ main() {
     git fetch k8s master
     
     mkdir -p tmp
-    docker run --rm -v "$(pwd):/workdir" --workdir /workdir "$IMAGE_REPOSITORY:$IMAGE_TAG" ct lint --config /workdir/.test/ct.yaml
+    docker run --rm -v "$(pwd):/workdir" --workdir /workdir "$CHART_TESTING_IMAGE:$CHART_TESTING_TAG" ct lint --config /workdir/.test/ct.yaml
 
     echo "Done Charts Linting!"
 }
