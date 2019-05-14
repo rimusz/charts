08f915cec86a15801938328e06c570484c4e6cc7
rimas <rmocius@gmail.com>
2019-05-14 11:38:21 +0100

    add local linting options

---
+++ .test/e2e-docker4mac.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+readonly IMAGE_TAG=${CHART_TESTING_TAG}
+readonly IMAGE_REPOSITORY="quay.io/helmpack/chart-testing"
+readonly CLUSTER_NAME=chart-testing
+
+run_ct_container() {
+    echo 'Running ct container...'
+    docker container run --rm --interactive --detach --name ct \
+        --volume "$(pwd):/workdir" \
+        --workdir /workdir \
+        "$IMAGE_REPOSITORY:$IMAGE_TAG"
+    echo
+}
+
+cleanup() {
+    echo 'Removing ct container...'
+    docker kill ct > /dev/null 2>&1
+
+    echo 'Done!'
+}
+
+docker_exec() {
+    docker exec --interactive -e HELM_HOST=127.0.0.1:44134 -e HELM_TILLER_SILENT=true ct "$@"
+}
+
+lookup_apiserver_container_id() {
+    docker container list --filter name=k8s_kube-apiserver --format '{{ .ID }}'
+}
+
+get_apiserver_arg() {
+    local container_id="$1"
+    local arg="$2"
+    docker container inspect "$container_id" | jq -r ".[].Args[] | capture(\"$arg=(?<arg>.*)\") | .arg"
+}
+
+connect_to_cluster() {
+    local apiserver_id
+    apiserver_id=$(lookup_apiserver_container_id)
+
+    if [[ -z "$apiserver_id" ]]; then
+        echo "ERROR: API-Server container not found. Make sure 'Show system containers' is enabled in Docker4Mac 'Preferences'!" >&2
+        exit 1
+    fi
+
+    local ip
+    ip=$(get_apiserver_arg "$apiserver_id" --advertise-address)
+
+    local port
+    port=$(get_apiserver_arg "$apiserver_id" --secure-port)
+
+    docker cp "$HOME/.kube" ct:/root/.kube
+    docker_exec kubectl config set-cluster docker-desktop "--server=https://$ip:$port"
+    docker_exec kubectl config set-cluster docker-desktop --insecure-skip-tls-verify=true
+    docker_exec kubectl config use-context docker-desktop
+}
+
+install_tiller() {
+     docker_exec apk add bash
+     echo "Install Tillerless Helm plugin..."
+     docker_exec helm init --client-only
+     docker_exec helm plugin install https://github.com/rimusz/helm-tiller
+     docker_exec bash -c 'echo "Starting Tiller..."; helm tiller start-ci >/dev/null 2>&1 &'
+     docker_exec bash -c 'echo "Waiting Tiller to launch on 44134..."; while ! nc -z localhost 44134; do sleep 1; done; echo "Tiller launched..."'
+     echo
+}
+
+install_charts() {
+    echo "Add git remote k8s ${CHARTS_REPO}"
+    git remote add k8s "${CHARTS_REPO}" &> /dev/null || true
+    git fetch k8s master
+    echo
+    # shellcheck disable=SC2086
+    docker_exec ct install ${CHART_TESTING_ARGS} --config /workdir/test/ct.yaml
+    echo
+}
+
+main() {
+    run_ct_container
+    trap cleanup EXIT
+
+    connect_to_cluster
+    install_tiller
+    install_charts
+}
+
+main
--- Makefile
+++ Makefile
@@ -1,4 +1,35 @@
 
+# Test charts locally
+MAC_ARGS ?=
+CHARTS_REPO ?= https://github.com/rimusz/charts
+CHART_TESTING_IMAGE ?= quay.io/helmpack/chart-testing
+CHART_TESTING_TAG ?= v2.3.3
+TEST_IMAGE_TAG ?= v3.3.2
+K8S_VERSION ?= v1.13.4
+KIND_VERSION ?= 0.2.1
+
+# If the first argument is "lint" or "mac" or "gke" or "kind"
+ifneq ( $(filter wordlist 1,lint mac gke kind), $(firstword $(MAKECMDGOALS)))
+  # use the rest as arguments for "lint" "mac" or "gke" or "kind"
+  MAC_ARGS := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+  # ...and turn them into do-nothing targets
+  $(eval $(MAC_ARGS):;@:)
+endif
+
+.PHONY: lint
+lint:
+	$(eval export CHART_TESTING_TAG)
+	$(eval export CHARTS_REPO)
+	$(eval export CHART_TESTING_ARGS=${MAC_ARGS})
+	@.test/lint-charts-local.sh
+
+.PHONY: mac
+mac:
+	$(eval export CHART_TESTING_TAG)
+	$(eval export CHARTS_REPO)
+	$(eval export CHART_TESTING_ARGS=${MAC_ARGS})
+	@.test/e2e-docker4mac.sh
+
 .PHONY: publish
 publish:
-	./.scripts/repo-sync.sh
+	@./.scripts/repo-sync.sh
