ff4cdf56128757f49efb93113361774cc03476b1
rimas <rmocius@gmail.com>
2020-06-17 13:06:51 +0300

    Signed-off-by: rimas <rmocius@gmail.com>

--- test/e2e-ci.sh
+++ test/e2e-ci.sh
@@ -57,7 +57,7 @@ create_kind_cluster() {
     chmod +x kind
     sudo mv kind /usr/local/bin/kind
 
-    kind create cluster --name "$CLUSTER_NAME" --config test/kind-config.yaml --image "kindest/node:$K8S_VERSION" --wait 60s
+    kind create cluster --name "$CLUSTER_NAME" --config --image "kindest/node:$K8S_VERSION" --wait 60s
     ### kind create cluster --name "$CLUSTER_NAME" --config test/kind-config.yaml --image "kindest/node:$K8S_VERSION" --wait 60s
 
     echo 'Copying kubeconfig to container...'
